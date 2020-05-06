aa7ef4bc2d7edce87d1afef79ccd13458cceaae9
dqn <suyarizzz@gmail.com>
2020-05-06 17:09:48 +0900

    feat: send message

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/dqn/twchatbot
 
 go 1.14
+
+require (
+	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f // indirect
+	github.com/dghubble/oauth1 v0.6.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,74 @@
 package main
 
-import "log"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Account struct {
+		ConsumerKey       string `yaml:"consumer_key"`
+		ConsumerSecret    string `yaml:"consumer_secret"`
+		AccessToken       string `yaml:"access_token"`
+		AccessTokenSecret string `yaml:"access_token_secret"`
+	} `yaml:"account"`
+}
+
+func loadConfig(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var c Config
+	err = yaml.Unmarshal(b, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
 
 func run() error {
+	c, err := loadConfig("./config.yml")
+	if err != nil {
+		return err
+	}
+
+	config := oauth1.NewConfig(c.Account.ConsumerKey, c.Account.ConsumerSecret)
+	token := oauth1.NewToken(c.Account.AccessToken, c.Account.AccessTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	_, _, err = client.DirectMessages.EventsNew(&twitter.DirectMessageEventsNewParams{
+		Event: &twitter.DirectMessageEvent{
+			Type: "message_create",
+			Message: &twitter.DirectMessageEventMessage{
+				Target: &twitter.DirectMessageTarget{
+					RecipientID: "1245969416694587393", // @R8472
+				},
+				Data: &twitter.DirectMessageData{
+					Text: "test",
+				},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
