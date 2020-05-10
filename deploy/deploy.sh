7a5adbbf681912f0fce853323051c25706be9f50
dqn <suyarizzz@gmail.com>
2020-05-11 04:04:15 +0900

    fix: deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,6 @@
 eval "$(cat ./deploy/.env <(echo) <(declare -x))"
 
 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w"
+ssh -t ${USER_NAME}@${HOST} -p ${PORT} "sudo systemctl stop twchatbot.service"
 scp -P ${PORT} twchatbot config.yml ${USER_NAME}@${HOST}:/home/${USER_NAME}/
 ssh -t ${USER_NAME}@${HOST} -p ${PORT} "sudo systemctl restart twchatbot.service"
